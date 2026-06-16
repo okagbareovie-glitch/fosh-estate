119a25cd5c003a2ecf0f2b31be56ae6fec488f29
xandersavage <47666196+xandersavage@users.noreply.github.com>
2026-06-16 06:46:03 +0100

    Remove site settings schema

--- sanity/schema/index.ts
+++ sanity/schema/index.ts
@@ -1,5 +1,4 @@
 import { listing } from "./listing";
-import { siteSettings } from "./site-settings";
 import { state } from "./state";
 
-export const schemaTypes = [listing, state, siteSettings];
+export const schemaTypes = [listing, state];
